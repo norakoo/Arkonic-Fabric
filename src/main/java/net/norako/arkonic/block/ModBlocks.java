422f3ae941848c588d6bed199cda30e66ac4a355
norakoo <nolanh76lh@gmail.com>
2023-02-17 17:35:29 +0100

    Added Enderling Brick & Bricks & all the variants.

--- src/main/java/net/norako/arkonic/block/ModBlocks.java
+++ src/main/java/net/norako/arkonic/block/ModBlocks.java
@@ -3,8 +3,7 @@ package net.norako.arkonic.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -18,6 +17,25 @@ public class ModBlocks {
     public static final Block ENDERLING_BRICKS = registerBlock("enderling_bricks",
             new Block(FabricBlockSettings.copyOf(Blocks.NETHER_BRICKS)), ItemGroups.BUILDING_BLOCKS);
 
+    public static final Block CRACKED_ENDERLING_BRICKS = registerBlock("cracked_enderling_bricks",
+            new Block(FabricBlockSettings.copyOf(Blocks.CRACKED_NETHER_BRICKS)), ItemGroups.BUILDING_BLOCKS);
+
+    public static final Block ENDERLING_BRICK_STAIRS = registerBlock("enderling_brick_stairs",
+            new StairsBlock(ModBlocks.ENDERLING_BRICKS.getDefaultState(),
+                    FabricBlockSettings.copyOf(Blocks.NETHER_BRICK_STAIRS)), ItemGroups.BUILDING_BLOCKS);
+
+    public static final Block ENDERLING_BRICK_SLAB = registerBlock("enderling_brick_slab",
+            new SlabBlock(FabricBlockSettings.copyOf(Blocks.NETHER_BRICK_SLAB)), ItemGroups.BUILDING_BLOCKS);
+
+    public static final Block ENDERLING_BRICK_WALL = registerBlock("enderling_brick_wall",
+            new WallBlock(FabricBlockSettings.copyOf(Blocks.NETHER_BRICK_WALL)), ItemGroups.BUILDING_BLOCKS);
+
+    public static final Block ENDERLING_BRICK_FENCE = registerBlock("enderling_brick_fence",
+            new FenceBlock(FabricBlockSettings.copyOf(Blocks.NETHER_BRICK_FENCE)), ItemGroups.BUILDING_BLOCKS);
+
+    public static final Block CHISELED_ENDERLING_BRICKS = registerBlock("chiseled_enderling_bricks",
+            new Block(FabricBlockSettings.copyOf(Blocks.CHISELED_NETHER_BRICKS)), ItemGroups.BUILDING_BLOCKS);
+
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
         return Registry.register(Registries.BLOCK, new Identifier(Arkonic.MOD_ID, name), block);
@@ -27,7 +45,13 @@ public class ModBlocks {
         Item item = Registry.register(Registries.ITEM, new Identifier(Arkonic.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings()));
         ItemGroupEvents.modifyEntriesEvent(group).register(entries -> {
-            entries.addAfter(Blocks.CHISELED_NETHER_BRICKS, item);
+            entries.addAfter(Blocks.RED_NETHER_BRICK_WALL, ModBlocks.ENDERLING_BRICKS);
+            entries.addAfter(ModBlocks.ENDERLING_BRICKS, ModBlocks.CRACKED_ENDERLING_BRICKS);
+            entries.addAfter(ModBlocks.CRACKED_ENDERLING_BRICKS, ModBlocks.ENDERLING_BRICK_STAIRS);
+            entries.addAfter(ModBlocks.ENDERLING_BRICK_STAIRS, ModBlocks.ENDERLING_BRICK_SLAB);
+            entries.addAfter(ModBlocks.ENDERLING_BRICK_SLAB, ModBlocks.ENDERLING_BRICK_WALL);
+            entries.addAfter(ModBlocks.ENDERLING_BRICK_WALL, ModBlocks.ENDERLING_BRICK_FENCE);
+            entries.addAfter(ModBlocks.ENDERLING_BRICK_FENCE, ModBlocks.CHISELED_ENDERLING_BRICKS);
         });
         return item;
     }
